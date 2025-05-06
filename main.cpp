2c94fdc179b9ca3df71d1e484a5acfceb9f8e3e7
hedgirun@gmail.com <hedgirun@gmail.com>
2025-05-06 18:29:53 +0300

    class Equipment added

--- main.cpp
+++ main.cpp
@@ -1,16 +1,10 @@
-#include "class2.h"
-#include "class3.h"
-#include "outsideClass.h"
+#include "Equipment.h"
 using namespace std;
 
 int main(){
-    Oleksiy::class1 obj1;
-    Oleksiy::class2 obj2;
-    Oleksiy::class3 obj3;
-    outsideClass obj4;
-
-    obj1.showInfo();
-    obj2.showInfo();
-    obj3.showInfo();
-    obj4.showInfo();
+    Products::Sport::Equipment obj1("Мяч", 699.99, "Китай", 5, "Футбол", 1);
+    obj1.show();
+    ofstream file("Shop.txt");
+    obj1.saveToFile(file);
+    file.close();
 }
